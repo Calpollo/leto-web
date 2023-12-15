cad12997501802c6be127da3645d588956ea3181
Andreas Nicklaus <andreas_nicklaus@web.de>
2023-12-15 11:09:57 +0100

    Importing Specific Icons instead of all

--- app/src/plugins/bootstrap-vue.js
+++ app/src/plugins/bootstrap-vue.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import { BootstrapVue, BIconBoxArrowUpRight, BIconPerson, BIconDoorOpen, BIconChevronDoubleUp, BIconChevronDoubleDown, BIconTrash, BIconTrashFill, BIconPlus, BIconX, BIconFilter, BIconInfo, BIconThreeDots, BIconKey, BIconArrowCounterclockwise, BIconFiles, BIconCheck, BIconCodeSquare, BIconEnvelope, BIconPhone, BIconPen, BIconExclamationTriangle, BIconDownload, BIconFolderPlus, BIconFolderSymlink, BIconCloudPlus, BIconPersonX, BIconPrinter, BIconExclamationCircle, BIconPieChart, BIconCardChecklist, BIconViewList, BIconFileEarmarkPdf, BIconUiChecks, BIconFilePdf, BIconArrowDown, BIconCalendar2Week, BIconJournalMedical, BIconBarChart, BIconArrowRight, BIconClockHistory, BIconTelephone, BIconPersonBadge, BIconCurrencyEuro, BIconCartCheck, BIconCartX } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+
 Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
+
+const icons = [BIconBoxArrowUpRight, BIconPerson, BIconDoorOpen, BIconChevronDoubleUp, BIconChevronDoubleDown, BIconTrash, BIconTrashFill, BIconPlus, BIconX, BIconFilter, BIconInfo, BIconThreeDots, BIconKey, BIconArrowCounterclockwise, BIconFiles, BIconCheck, BIconCodeSquare, BIconEnvelope, BIconPhone, BIconPen, BIconExclamationTriangle, BIconDownload, BIconFolderPlus, BIconFolderSymlink, BIconCloudPlus, BIconPersonX, BIconPrinter, BIconExclamationCircle, BIconPieChart, BIconCardChecklist, BIconViewList, BIconFileEarmarkPdf, BIconUiChecks, BIconFilePdf, BIconArrowDown, BIconCalendar2Week, BIconJournalMedical, BIconBarChart, BIconArrowRight, BIconClockHistory, BIconTelephone, BIconPersonBadge, BIconCurrencyEuro, BIconCartCheck, BIconCartX]
+icons.forEach(icon => {
+  const componentString = icon.options.name.replace(/([A-Z])/g, " $1").split(' ').slice(1).join('-').toLowerCase()
+  Vue.component(componentString, icon)
+})
